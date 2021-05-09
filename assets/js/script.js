01e786aa8d20e274aea8356df5bc6fd1a801b388
SpeedRacer <robertgeorge2@gmail.com>
2021-05-08 22:41:03 -0400

    Got HTML and CSS layout ready. Using local Bootstrap for 5.0.0. Set up variables in HTML and in JS.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var userInputCity = document.getElementById("userInputCity");
+var searchButton = document.getElementById("searchButton");
+var previousCities = document.getElementById("previousCities");
+var userCity = document.getElementById("userCity");
+var weatherIcon = document.getElementById("weatherIcon");
+var temp0 = document.getElementById("temp0");
+var wind0 = document.getElementById("wind0");
+var humidity0 = document.getElementById("humidity0");
+var uv0 = document.getElementById("uv0");
+
+var searchButton = document.getElementById("searchButton");
