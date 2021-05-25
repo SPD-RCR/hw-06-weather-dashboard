0277ea03760b74dafacecf6511f363ccc76c54d1
SpeedRacer <robertgeorge2@gmail.com>
2021-05-25 16:26:35 -0400

    Working on converting userCityLatLonWether.daily .dt from seconds to string to display date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var searchButton = document.getElementById("searchButton");
 var previousCities = document.getElementById("previousCities");
 
 var userCity = document.getElementById("userCity");
+var date0 = document.getElementById("date0");
 var weatherIcon0 = document.getElementById("weatherIcon0");
 var temp0 = document.getElementById("temp0");
 var wind0 = document.getElementById("wind0");
@@ -64,57 +65,82 @@ var getCityNameWeather = function (cityName) {
 //     getCityLatLonWeather (userCityWeather, cityName);
 //   };
 
-  var getCityLatLonWeather = function(userCityWeather, cityName) {
-    var lat = userCityWeather.coord.lat;
-    console.log('lat:', lat)
-    var lon = userCityWeather.coord.lon;
-    console.log('lon:', lon)
-    // One Call API
-    var apiCityLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + 'db0bba3ceabf7ca8a120908817237044';
-      console.log("apiCityLatLonUrl: ", apiCityLatLonUrl);
-      console.log("cityName72: ", cityName);
+var getCityLatLonWeather = function(userCityWeather, cityName) {
+  var lat = userCityWeather.coord.lat;
+  console.log('lat:', lat)
+  var lon = userCityWeather.coord.lon;
+  console.log('lon:', lon)
+  // One Call API
+  var apiCityLatLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + 'db0bba3ceabf7ca8a120908817237044';
+    console.log("apiCityLatLonUrl: ", apiCityLatLonUrl);
+    console.log("cityName72: ", cityName);
 
-      fetch(apiCityLatLonUrl)
-      .then(function (response) {
-        if (response.ok) {
-                      console.log("fetch if response:", response)
-          response.json().then(function (data) {
-                      console.log("fetch response json data:", data)
-                      console.log("cityName82:", cityName)
-          displayUserCityLatLonWeather (data, cityName);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-      }
-      })
-      .catch(function (error) {
-      alert('Unable to connect to Weather API');
-      });
-  };
-
-  var displayUserCityLatLonWeather = function (userCityLatLonWeather, cityName) {
-    if (userCityLatLonWeather.length === 0) {
-        userCity.textContent = 'No weather data found.';
-      // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-      return;
+    fetch(apiCityLatLonUrl)
+    .then(function (response) {
+      if (response.ok) {
+                    console.log("fetch if response:", response)
+        response.json().then(function (data) {
+                    console.log("fetch response json data:", data)
+                    console.log("cityName82:", cityName)
+        displayUserCityLatLonWeather (data, cityName);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
     }
-  
-    userCity.textContent = cityName;
-    weatherIcon0.setAttribute("src",`https://openweathermap.org/img/w/${userCityLatLonWeather.current.weather[0].icon}.png`);
-    temp0.textContent = userCityLatLonWeather.current.temp;
-    wind0.textContent = userCityLatLonWeather.current.wind_speed;
-    humidity0.textContent = userCityLatLonWeather.current.humidity;
-    uv0.textContent = userCityLatLonWeather.current.uvi; 
+    })
+    .catch(function (error) {
+    alert('Unable to connect to Weather API');
+    });
+};
 
-    for (let i = 0; i < userCityLatLonWeather.length; i++) {
-      //const element = UserCityWeather[i];
+var displayUserCityLatLonWeather = function (userCityLatLonWeather, cityName) {
+  if (userCityLatLonWeather.length === 0) {
+      userCity.textContent = 'No weather data found.';
+    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+    return;
+  }
 
-      // userCity.textContent = userCityLatLonWeather[i].name;
-      // console.log("userCity", userCity.textContent);
+  userCity.textContent = cityName;
+  date0.textContent = userCityLatLonWeather.daily[0].dt; // Current date in seconds UNIX = ERROR = NEED to convert value to String
+  // date0.textContent = dateFormatted(userCityLatLonWeather.daily[0].dt); // Current date in seconds UNIX 
+  weatherIcon0.setAttribute("src",`https://openweathermap.org/img/w/${userCityLatLonWeather.current.weather[0].icon}.png`);
+  temp0.textContent = userCityLatLonWeather.current.temp;
+  wind0.textContent = userCityLatLonWeather.current.wind_speed;
+  humidity0.textContent = userCityLatLonWeather.current.humidity;
+  uv0.textContent = userCityLatLonWeather.current.uvi; 
 
-      temp.textContent = userCityLatLonWeather[i].temp;
-      console.log("TEST:", "TEST")
-    }
-  };
+  for (let i = 0; i < userCityLatLonWeather.length; i++) {
+    //const element = userCityLatLonWeather[i];
+
+    // userCity.textContent = userCityLatLonWeather[i].name;
+    // console.log("userCity", userCity.textContent);
+
+    temp.textContent = userCityLatLonWeather[i].temp;
+    console.log("TEST:", "TEST")
+  }
+};
+
+
+
+// var dateFormatted = function (dt) {
+//   const unixTimestamp = dt.value; //1575909015
+
+//   const milliseconds = unixTimestamp * 1000 // 1575909015000
+
+//   const dateObject = new Date(milliseconds)
+
+//   const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+//   console.log('humanDateFormat:', humanDateFormat)
+
+//   dateObject.toLocaleString("en-US", {weekday: "long"}) // Monday
+//   dateObject.toLocaleString("en-US", {month: "long"}) // December
+//   dateObject.toLocaleString("en-US", {day: "numeric"}) // 9
+//   dateObject.toLocaleString("en-US", {year: "numeric"}) // 2019
+//   dateObject.toLocaleString("en-US", {hour: "numeric"}) // 10 AM
+//   dateObject.toLocaleString("en-US", {minute: "numeric"}) // 30
+//   dateObject.toLocaleString("en-US", {second: "numeric"}) // 15
+//   dateObject.toLocaleString("en-US", {timeZoneName: "short"}) // 12/9/2019, 10:30:15 AM CST
+// };
+// console.log("dateFormatted: ", dateFormatted);
 
 document.getElementById("searchButton").addEventListener("click", formSubmitHandler);
